e5f61b9401e71a3cfc0349bea4025e62e9c9a943
EleanorColvin <99199758+EleanorColvin@users.noreply.github.com>
2022-11-03 21:21:00 -0400

    Add files via upload

---
+++ IntroToJS/script.js
@@ -0,0 +1,53 @@
+function greet()
+{
+    console.log("Hello World");
+}
+function shoutTen()
+{
+    console.log("10!");
+    shoutNine();
+} function shoutNine()
+{
+    console.log("9!");
+    shoutEight();
+} 
+function shoutEight()
+{
+    console.log("8!");
+    shoutSeven();
+} 
+function shoutSeven()
+{
+    console.log("7!");
+    shoutSix();
+} 
+function shoutSix()
+{
+    console.log("6!");
+    shoutFive();
+
+} 
+function shoutFive()
+{
+    console.log("5!");
+    shoutFour();
+} 
+function shoutFour()
+{
+    console.log("4!");
+    shoutThree();
+} 
+function shoutThree()
+{
+    console.log("3!");
+    shoutTwo();
+} 
+function shoutTwo()
+{
+    console.log("2!");
+    shoutOne();
+}  
+function shoutOne()
+{
+    console.log("1!");
+}  
---
+++ IntroToJS/script2.js
@@ -0,0 +1,26 @@
+function initialize()
+{
+    num1 = 3;
+    num2 = 7;
+}
+
+function addNumbers()
+{
+    var result = num1 + num2;
+    console.log(result);
+}
+function subtractNumbers()
+{
+    var result = num1 - num2;
+    console.log(result);
+}
+function multiplyNumbers()
+{
+    var result = num1 * num2;
+    console.log(result);
+}
+function divideNumbers()
+{
+    var result = num1 / num2;
+    console.log(result);
+}
